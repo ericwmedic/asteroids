d6a44abcf26ce42b6875e3b157a7b906bd7fb280
ericwmedic <ericwmedic@gmail.com>
2024-10-24 21:51:43 -0400

    player sprite

--- player.py
+++ player.py
@@ -1,9 +1,15 @@
+import pygame
+from constants import *
+from circleshape import CircleShape
+
 def Player(Circleshape):
     def __init__(self,x,y):
-        super().__init__(self.radius)
-        rotation = 0
+        super().__init__(x, y, PLAYER_RADIUS)
+        self.rotation = 0
 
-    # in the player class
+    def draw(self, screen):
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
